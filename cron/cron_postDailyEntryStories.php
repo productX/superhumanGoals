71704535fd625848ee60c8ccfae9540a71d2bdb1
System Administrator <root@Adams-MacBook-Air-2.local>
2012-03-26 11:57:49 -0700

    added chmod changes across the board

--- cron/cron_postDailyEntryStories.php
+++ cron/cron_postDailyEntryStories.php
@@ -6,10 +6,10 @@ CRON INSTRUCTIONS:
 
 include("../template/baseIncludes.php");
 
-$rs = $db->doQuery("SELECT id FROM users WHERE (".Date::Now()->toUT()."-UNIX_TIMESTAMP(last_daily_entry))>(60*30) AND daily_entry_story_posted=FALSE");
+$rs = $db->doQuery("SELECT user_id FROM users WHERE (".Date::Now()->toUT()."-UNIX_TIMESTAMP(last_daily_entry))>(60*30) AND daily_entry_story_posted=FALSE");
 $obj = null;
 while($obj = mysql_fetch_object($rs)) {
-	$userID = $obj->id;
+	$userID = $obj->user_id;
 	$touchedGoalIDs = array();
 	$today = Date::now()->toDay();
 	$rs2 = $db->doQuery("SELECT event_goal_id FROM stories WHERE user_id=%s AND is_public=TRUE AND type='event' AND entered_at_day=%s", $userID, $today);
@@ -17,7 +17,7 @@ while($obj = mysql_fetch_object($rs)) {
 	while($obj2 = mysql_fetch_object($rs2)) {
 		$touchedGoalIDs[$obj2->event_goal_id]=true;
 	}
-	$rs2 = $db->doQuery("SELECT dailytests.goal_id FROM dailytests_status INNER JOIN dailytests ON dailytests.id=dailytests_status.dailytest_id WHERE dailytests_status.user_id=%s AND dailytests_status.entered_at_day=%s", $userID, $today);
+	$rs2 = $db->doQuery("SELECT strategies.goal_id FROM strategies_log INNER JOIN strategies ON strategies.strategy_id=strategies_log.strategy_id WHERE strategies_log.user_id=%s AND strategies_log.entered_at_day=%s", $userID, $today);
 	$obj2 = null;
 	while($obj2 = mysql_fetch_object($rs2)) {
 		$touchedGoalIDs[$obj2->goal_id]=true;
