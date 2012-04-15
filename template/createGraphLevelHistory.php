d30a480532af002e6eddbb90e3570d96131e21c1
Roger Dickey <roger@productx.co>
2012-04-14 21:29:53 -0700

    fixes for: - populate help page - graph broken on goal page - log-out link broken - Fix user pic overlap on activity tab of user page - change adherence to habit everywhere - feed stories should be more compact - edit tab on goals page should only be default if going through all goals page

--- template/createGraphLevelHistory.php
+++ template/createGraphLevelHistory.php
@@ -1,15 +1,6 @@
 <?php
 include("userFacingForceLogin.php");
 
-/*
-Alternative graph libraries:
-
-- http://omnipotent.net/jquery.sparkline/
-*/
-
-// include "PHP graph lib"
-require_once("../../common/lib/phpgraphlib/phpgraphlib.php");
-
 // verify & pull parameters
 if(	!isset($_GET["userID"]) ||
 	!isset($_GET["goalID"])) {
@@ -26,27 +17,15 @@ $graphData = EventStory::getLevelHistory($userID, $goalID, $numDaysBack);
 $width = 200;
 $height = 40;
 if(isset($_GET['big'])) {
-	$width = 450;
+	$width = 400;
 	$height = 100;
 }
 
-// set up the graph library & output the appropriate headers/binary for the graph
-$graph = new PHPGraphLib($width,$height);
-/*$data = array("1" => .0032, "2" => .0028, "3" => .0021, "4" => .0033, 
-"5" => .0034, "6" => .0031, "7" => .0036, "8" => .0027, "9" => .0024, 
-"10" => .0021, "11" => .0026, "12" => .0024, "13" => .0036, 
-"14" => .0028, "15" => .0025);*/
-$graph->addData($graphData);
-//$graph->setTitle("Progress: Last ".$numDaysBack." days");
-$graph->setBars(false);
-$graph->setLine(true);
-$graph->setRange(10,0);
-$graph->setXValues(false);
-/*$graph->setDataPoints(true);
-$graph->setDataPointColor('maroon');*/
-//$graph->setDataValues(true);
-$graph->setDataValueColor('maroon');
-/*$graph->setGoalLine(.0025);
-$graph->setGoalLineColor('red');*/
-$graph->createGraph();
+
 ?>
+<span class="levelGraph"></span>
+<script type="text/javascript">
+	/* Inline sparklines take their values from the contents of the tag */
+	var values = [<?php echo implode(",",$graphData); ?>];
+	$('.levelGraph').sparkline(values, {width:'<?php echo $width;?>px',height:'<?php echo $height;?>px', chartRangeMin:0, chartRangeMax:10, lineWidth:4, spotRadius:8}); 
+</script>
